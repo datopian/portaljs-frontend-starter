8d9e235be328453fe9a413778c16b56be0fe28e9
William Lima <willy.lima89@gmail.com>
2024-12-06 10:56:58 -0100

    add particles to the lighter theme

--- themes/lighter/layout.tsx
+++ themes/lighter/layout.tsx
@@ -1,6 +1,9 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useCallback } from "react";
 import styles from "./styles.module.scss";
 import { Inter } from "next/font/google";
+import Particles from "react-particles";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,9 +20,105 @@ const LigtherTheme = ({
   Footer?: FC;
   children: ReactNode;
 }) => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    //await loadFull(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
     <>
-      <div className={` ${styles.LightTheme} ${inter.className}`}>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          particles: {
+            destroy: {
+              mode: "split",
+              split: {
+                count: 1,
+                factor: {
+                  value: {
+                    min: 2,
+                    max: 4,
+                  },
+                },
+                rate: {
+                  value: 100,
+                },
+                particles: {
+                  life: {
+                    count: 1,
+                    duration: {
+                      value: {
+                        min: 2,
+                        max: 3,
+                      },
+                    },
+                  },
+                  move: {
+                    speed: {
+                      min: 10,
+                      max: 15,
+                    },
+                  },
+                },
+              },
+            },
+            number: {
+              value: 10,
+            },
+            color: {
+              value: ["#00DEF2", "#6990BE", "#00375F"],
+            },
+            shape: {
+              type: "circle",
+            },
+            opacity: {
+              value: 0,
+            },
+            stroke: {
+              width: 1,
+            },
+
+            size: {
+              value: {
+                min: 2,
+                max: 4,
+              },
+            },
+            collisions: {
+              enable: true,
+              mode: "bounce",
+            },
+            move: {
+              enable: true,
+              speed: 1,
+              outModes: "bounce",
+            },
+          },
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "pop",
+              },
+            },
+          },
+          background: {
+            color: "transparent",
+          },
+        }}
+      />
+      <div className={` ${styles.LightTheme} ${inter.className} relative`}>
         {Header && <Header />}
         <div className="content-wrapper">
           {Sidebar && <Sidebar />}
