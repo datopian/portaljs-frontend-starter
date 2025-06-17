974340e5fbb0b242493ad9609ac44c2ee776bfca
Datopian-Shreyas <shreyas.muniyappa@datopian.com>
2025-06-17 16:18:49 +0530

    [seo][lg]: central SEO config file created

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,25 +1,74 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+const title = "PortalJS frontend template";
+const description =
+  "Discover thousands of datasets, publish your own, and request data via Portal – an open data platform powered by PortalJS.";
+
+const url = "https://portaljs-cloud-frontend-template.vercel.app/";
+const imageUrl = `${url}images/portaljs-frontend.png`;
+
 export default {
-  titleTemplate: "%s | Ckan Homepage",
-  description: "Ckan Homepage",
-  canonical: "https://datahub-enterprise.vercel.app/",
+  title,
+  titleTemplate: "%s | PortalJS",
+  description,
+  canonical: url,
   openGraph: {
-    title: "Ckan Homepage",
+    title,
+    description,
     type: "website",
-    url: "https://datahub-enterprise.vercel.app/",
-    site_name: "Ckan Homepage",
+    locale: "en_US",
+    url,
+    site_name: title,
     images: [
       {
-        url: "https://datahub-enterprise.vercel.app/images/datahub_enterprise_frontend.png",
-        alt: "Ckan Homepage",
+        url: imageUrl,
+        alt: title,
         width: 1200,
         height: 627,
-        type: "image/jpg",
+        type: "image/png",
       },
     ],
   },
-  // twitter: {
-  //   handle: "@datahubenterprise",
-  //   site: "https://datahub-enterprise.vercel.app/",
-  //   cardType: "summary_large_image",
-  // },
+  twitter: {
+    handle: "@datopian",
+    site: "@PortalJS_",
+    cardType: "summary_large_image",
+  },
+  additionalMetaTags: [
+    {
+      name: "keywords",
+      content: "PortalJS, open data, datasets, data portal, Portal, datopian, frontend template",
+    },
+    {
+      name: "author",
+      content: "Datopian / PortalJS",
+    },
+    {
+      property: "og:image:width",
+      content: "1200",
+    },
+    {
+      property: "og:image:height",
+      content: "627",
+    },
+    {
+      property: "og:locale",
+      content: "en_US",
+    },
+  ],
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+    },
+    {
+      rel: "apple-touch-icon",
+      href: "/apple-touch-icon.png",
+      sizes: "180x180",
+    },
+    {
+      rel: "manifest",
+      href: "/site.webmanifest",
+    },
+  ]
 };
