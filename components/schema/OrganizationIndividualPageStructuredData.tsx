61bf865e35114bd60e60f1435b59e22981401e6f
Datopian-Shreyas <shreyas.muniyappa@datopian.com>
2025-06-19 16:32:27 +0530

    [seo][md]: added jsonLd for org individual page

---
+++ components/schema/OrganizationIndividualPageStructuredData.tsx
@@ -0,0 +1,41 @@
+import nextSeoConfig, { url } from "@/next-seo.config";
+import { BreadcrumbJsonLd, LogoJsonLd, NextSeo, WebPageJsonLd, SiteLinksSearchBoxJsonLd } from "next-seo";
+
+export function OrganizationIndividualPageStructuredData({ org }) {
+  const title = org.name || org.title
+  const description = org.notes || "Organizations page of " + org
+  return (
+    <>
+      <LogoJsonLd
+        url={`${url}/@${title}`}
+        logo={org.image_display_url || `${url}/favicon.ico`}
+      />
+      <NextSeo
+        canonical={`${url}/@${title}`}
+        title={title}
+        description={description}
+        {...nextSeoConfig}
+      />
+      <BreadcrumbJsonLd
+        itemListElements={[
+          {
+            position: 1,
+            name: 'Home',
+            item: url,
+          },
+          {
+            position: 2,
+            name: 'Organizations',
+            item: `${url}/@${title}`,
+          },
+        ]}
+      />
+      <WebPageJsonLd
+        id={`${url}/@${title}#webpage`}
+        url={`${url}/@${title}`}
+        name={title}
+        description={description}
+      />
+    </>
+  );
+}
