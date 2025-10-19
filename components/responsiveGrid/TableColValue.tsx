f2fb1d0873d1c353ac667fd2577f812c8c35f916
William Lima <willy.lima89@gmail.com>
2025-10-19 21:51:29 -0100

    Fix table boolean value preview

--- components/responsiveGrid/TableColValue.tsx
+++ components/responsiveGrid/TableColValue.tsx
@@ -4,6 +4,8 @@ export default function TableColumnValue({ column, value }) {
   const { visibleColumns, pinnedColumns } = useResourceData();
   const isVisible = visibleColumns.includes(column);
   const isPinned = pinnedColumns.includes(column);
+  const _value = typeof value=== "boolean" ? value.toString() : value;
+
   return (
     <td
       className={` px-3 py-4 text-sm text-gray-500   ${
@@ -13,7 +15,7 @@ export default function TableColumnValue({ column, value }) {
       tabIndex={0}
       aria-label={value}
     >
-      <span className=" block max-w-[400px] w-[max-content] ">{value}</span>
+      <span className=" block max-w-[400px] w-[max-content] ">{_value}</span>
       {isPinned && (
         <span className="absolute right-[-1px] h-full w-[1px] bg-gray-100 top-0"></span>
       )}
